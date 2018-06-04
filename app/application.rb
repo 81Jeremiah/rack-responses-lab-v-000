ccbaf7c1e5d65a540130e2dfe427b373150b4579
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-06-04 01:10:22 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,9 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.new
+    if time.hour > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
